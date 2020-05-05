d9dacac452858605f927cb197bd5c49a660a5bcc
JuanDavidDava2 <juandm417@gmail.com>
2020-05-05 04:38:24 +0000

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+# This script get the todo list from a given userId
 import json
 import requests
 
-if __name__ == '__main__':
-
-    url = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
@@ -17,6 +13,6 @@ if __name__ == '__main__':
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": u.get("username")
-             } for t in requests.get(url + "todos",
-                                     params={"userId": u.get("id")}).json()]
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
             for u in users}, jsonfile)
