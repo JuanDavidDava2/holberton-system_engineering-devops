4ae7dd973889671bb35eb102e6eb7c5c34dd6d5b
JuanDavidDava2 <juandm417@gmail.com>
2020-05-20 18:18:43 +0000

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fix error and change ".phpp" to ".php"
+exec {'fix_with_sed':
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
